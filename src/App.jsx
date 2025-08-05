8a51f7ada38c082e0b71583d6b1970712418e87a
Azure <paramasuraqutay@gmail.com>
2025-08-05 11:55:12 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>}/>)
+)
 
 const App = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Hero title='Bankai' subtitle='Senbounzakura Kageyoshi'/>
-      <HomeCards/>
-    </div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
