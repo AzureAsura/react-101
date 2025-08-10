b13ac0b356ba4e08ca8777e7221c0fc5b33a893b
Azure <paramasuraqutay@gmail.com>
2025-08-10 21:35:43 +0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
 
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/index.html">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
                 src={logo}
